2c93dee05d533f20633b2ef97e53e3230de05334
Windel Navales <windelvlog@gmail.com>
2024-11-02 12:57:16 -0700

    up

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,11 @@
-const audio = document.getElementById('background-music');
-audio.onerror = function() {
-    console.error('Error loading audio file.');
-};
+   // Create a SoundCloud player
+   var iframeElement = document.createElement('iframe');
+   iframeElement.style.display = 'none'; // Hide the iframe
+   iframeElement.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1948137423&auto_play=true&show_user=false';
+   document.body.appendChild(iframeElement);
+
+   // Wait for the iframe to load
+   iframeElement.onload = function() {
+       var widget = SC.Widget(iframeElement);
+       widget.play(); // Play the audio
+   };
